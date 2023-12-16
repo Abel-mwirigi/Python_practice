6d5122c34426fdb744740211eb2bd156c6f7a557
Abel-mwirigi <mwirigiabel223359@daystar.ac.ke>
2023-12-16 21:34:04 +0300

    list slicing

---
+++ players.py
@@ -0,0 +1,9 @@
+#slicing a list
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3])
+
+"""
+If you omit the first index in a slice, Python automatically starts your 
+slice at the beginning of the list:
+"""
+print(players[:4])
