01929e6662018b84dbb6e36dfb8b5ff2a3012f70
Abel-mwirigi <mwirigiabel223359@daystar.ac.ke>
2023-12-16 15:59:56 +0300

    reverse:function to enable sorting in reverse

--- cars.py
+++ cars.py
@@ -1,6 +1,16 @@
 cars = ['bmw', 'audi', 'toyota', 'subaru']
 """
 sort funtion sort the list of items in alphabetical order
+and we can never revert to 
+the original order
 """
 cars.sort()
+print(cars)
+
+"""
+reverse=true
+function to enable reversing items in reverse alphabetical
+order
+"""
+cars.sort(reverse=True)
 print(cars)
